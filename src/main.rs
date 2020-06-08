4dfae8e0ea37d4e2b0080981e6b14dc1f4c6c34c
abhikjain360 <abhikjain360@gmail.com>
2020-06-08 09:43:36 +0530

    +tests

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,7 @@ mod matrix;
 use matrix::Matrix;
 
 fn main() {
-    let (a, b): (Matrix<f64>, Matrix<f64>) = input::get_input();
-    a.print();
-    b.print();
-    let mut c = a.clone() + b.clone();
-    c.print();
-    c = matrix::normal_multiplication(&a, &b);
-    c.print();
+    let (mut a, b): (Matrix<f64>, Matrix<f64>) = input::get_input();
+    a.fill_zeroes(4);
     a.print();
 }
