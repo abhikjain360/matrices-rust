e33898285a6f0d21907b98e8edd8785422b056eb
abhikjain360 <abhikjain360@gmail.com>
2020-06-09 09:51:35 +0530

    comparision

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
+#![allow(unused_variables)]
 mod input;
 mod matrix;
+mod tests;
 
 use matrix::Matrix;
 
 fn main() {
-    let (a, b): (Matrix<f64>, Matrix<f64>) = input::get_input();
+    let (a, b): (Matrix<i32>, Matrix<i32>) = tests::create_test_matrices();
+    //    a.print();
+    //    b.print();
     let c = a.clone() * b.clone();
-    c.print();
-    let c = matrix::multiplication_normal(&a, &b);
-    c.print();
+    //    c.print();
+    //let c = matrix::multiplication_normal(&a.clone(), &b.clone());
+    //   c.print();
 }
